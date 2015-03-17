bf27411ac021fcf745480cb8f93de8e079cb4eea
Nazariglez <nazari.nz@gmail.com>
2015-03-17 16:33:46 +0000

    added basic ajax request to gamedonia server

--- src/Gamedonia.js
+++ src/Gamedonia.js
@@ -1,6 +1,7 @@
 var superAgent = require('superagent'),
     UserManager = require('./UserManager'),
     DataManager = require('./DataManager'),
+    cryptoHex = require('crypto-js').enc.Hex,
     MD5 = require('crypto-js/md5'),
     HmacSHA1= require('crypto-js/hmac-sha1');
 
@@ -20,8 +21,53 @@ function Gamedonia(gameSecret, apiKey, url, version){
 
 Gamedonia.prototype.constructor = Gamedonia;
 
-Gamedonia.prototype.ajax = function(url, data, method, headers, callback){
+Gamedonia.prototype.request = function(url, data, method, callback){
+    var date = new Date().toUTCString(),
+        contentType = "application/json";
 
+    var request;
+    switch(method){
+        case "POST":
+            request = superAgent.post(this.apiURL + url);
+            break;
+        case "GET":
+            request = superAgent.get(this.apiURL + url);
+            break;
+        case "PUT":
+            request = superAgent.put(this.apiURL + url);
+            break;
+        case "DELETE":
+            request = superAgent.del(this.apiURL + url);
+            break;
+    }
+
+    request.data(data)
+        .set('X-Date', date)
+        .set('X-Gamedonia-ApiKey', this.apiKey)
+        .set('Content-type', contentType)
+        .set('X-Gamedonia-Signature', this._getSignature(url, data, method, contentType, date))
+        .send(data)
+        .end(callback);
+};
+
+Gamedonia.prototype._getSignature = function(url, data, method, contentType, date){
+    if(method === "POST" || method === "PUT"){
+        return sign(this.gameSecret, JSON.stringify(data), contentType, date, method, url);
+    }else{
+        return signGetOrDelete(this.gameSecret, date, method, url);
+    }
 };
 
 module.exports = Gamedonia;
+
+function sign(secret,data,contentType,date,requestMethod, path){
+    var contentMd5 = MD5(data).toString(cryptoHex);
+    var toSign = requestMethod + "\n" + contentMd5 + "\n" + contentType + "\n" + date + "\n" + path;
+    var calculatedSignature = HmacSHA1(toSign,secret);
+    return calculatedSignature.toString(cryptoHex);
+}
+function signGetOrDelete(secret,date,requestMethod, path){
+    var toSign = requestMethod + "\n" + date + "\n" + path;
+    var calculatedSignature = HmacSHA1(toSign,secret);
+    return calculatedSignature.toString(cryptoHex);
+}
